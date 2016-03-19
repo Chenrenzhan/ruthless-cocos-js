94c6efbdd80e2d0b40a11eb21624c70045b1a4d8
Chenrenzhan <chenrenzhan@gmail.com>
2016-03-19 23:37:53 +0800

    cocos命令行创建js项目

---
+++ ruthless/src/resource.js
@@ -0,0 +1,22 @@
+var res = {
+    //HelloWorld_png : "res/HelloWorld.png",
+    // ui image resource
+    igMainBg : "res/ui/main_bg.png",
+    igDialogBg : "res/ui/dialog_bg.png",
+    igTitle : "res/ui/title.png",
+        //loading-page
+    igBarBg : "res/ui/loading-page/bar_background.png",
+    igBarFront : "res/ui/loading-page/bar_front.png",
+    igBarSlider : "res/ui/loading-page/garlic.png",
+    igTipFrame : "res/ui/loading-page/tip_frame.png",
+        // main-page
+    igBtnArcade : "res/ui/main-page/btn_arcade.png",
+    igBtnChallenge : "res/ui/main-page/btn_challenge.png",
+    igBtnClassics : "res/ui/main-page/btn_classics.png",
+    igBtnMenu : "res/ui/main-page/btn_menu.png",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
